c2fa29c45edda093963369534787a41e2943901f
Thomas Dierickx <thomas.dierickx3@student.howest.be>
2022-10-24 13:26:57 +0200

    small spacing fix

--- src/js/main.ts
+++ src/js/main.ts
@@ -2,17 +2,6 @@ const init = () => {
 
 }
 
-const $shadow = document.getElementById('shadow-person');
-
-window.addEventListener('mousemove', (e) => {
-  const mouseX = e.pageX;
-  const mouseY = e.pageY;
-  // Do something with these coordinates
-  let left = e.offsetX;
-
-  $shadow.style.left = left + 'px';
-});
-
 const update = (e) => {
   let x = e.clientX
   let y = e.clientY
@@ -21,8 +10,16 @@ const update = (e) => {
   document.documentElement.style.setProperty('--cursorY', y + 'px')
 }
 
-document.addEventListener('mousemove',update)
-document.addEventListener('touchmove',update)
+const $shadow = document.getElementById('shadow-person');
+window.addEventListener('mousemove', (e) => {
+  // Makes shadow follow cursor
+  let left = e.offsetX;
+
+  $shadow.style.left = left + 'px';
+});
+
+document.addEventListener('mousemove',update);
+document.addEventListener('touchmove',update);
 
 init()
 postMessage({ payload: 'removeLoading' }, '*')
